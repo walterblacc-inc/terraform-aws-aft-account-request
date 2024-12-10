8c1582310bc21cef7d76808dcde78034b214b251
Pierre Thelusma <pthelusma@gmail.com>
2024-12-10 10:24:33 -0500

    Creating sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "me+walterblacc-aws-sandbox@walterblacc.com"
+    AccountName               = "walterblacc-sandbox"
+    ManagedOrganizationalUnit = "walterblacc-account-factory"
+    SSOUserEmail              = "me+walterblacc-aws-account-factory@walterblacc.com"
+    SSOUserFirstName          = "Walter"
+    SSOUserLastName           = "Blacc"
   }
 
   account_tags = {
@@ -23,5 +23,5 @@ module "sandbox" {
     group = "non-prod"
   }
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = "walterblacc-sandbox"
 }
